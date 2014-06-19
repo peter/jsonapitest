d8c78c38aa94662e90543cc47ac82b073e1894b9
Peter Marklund <peter@marklunds.com>
2014-06-19 19:29:23 +0200

    Refactoring api_call.parse making some of the transformation functions more cohesive and making sure they are invoked in the appropriate order. Validation comes last

--- test/api_call_test.js
+++ test/api_call_test.js
@@ -76,13 +76,25 @@ describe('api_call', function() {
   });
 
   describe('normalizeRequest', function() {
+    it('combines path and base_url into a url', function() {
+      assert.deepEqual(apiCall.normalizeRequest({request: {base_url: 'http://foobar.com', path: '/bla'}}), {request: {method: 'GET', url: 'http://foobar.com/bla'}})
+      assert.deepEqual(apiCall.normalizeRequest({request: {base_url: 'http://foobar.com', url: 'http://otherdomain.com/bla'}}), {request: {method: 'GET', url: 'http://otherdomain.com/bla'}})
+      assert.deepEqual(apiCall.normalizeRequest({request: {method: 'POST', base_url: 'http://foobar.com', url: 'http://otherdomain.com/bla'}}), {request: {method: 'POST', url: 'http://otherdomain.com/bla'}})
+    });
+    it('defaults method to GET', function() {
+      assert.deepEqual(apiCall.normalizeRequest({request: {url: 'http://foobar.com'}}), {request: {method: 'GET', url: 'http://foobar.com'}})
+      assert.deepEqual(apiCall.normalizeRequest({request: {method: 'DELETE', url: 'http://foobar.com'}}), {request: {method: 'DELETE', url: 'http://foobar.com'}})
+    });
+  });
+
+  describe('parseRequestString', function() {
     it('can parse a request string', function() {
-      assert.deepEqual(apiCall.normalizeRequest({request: '/foo/bar?bla=1'}), {request: {method: 'GET', path: '/foo/bar?bla=1'}});
-      assert.deepEqual(apiCall.normalizeRequest({request: 'DELETE /foo/bar?bla=1'}), {request: {method: 'DELETE', path: '/foo/bar?bla=1'}});
+      assert.deepEqual(apiCall.parseRequestString({request: '/foo/bar?bla=1'}), {request: {path: '/foo/bar?bla=1'}});
+      assert.deepEqual(apiCall.parseRequestString({request: 'DELETE /foo/bar?bla=1'}), {request: {method: 'DELETE', path: '/foo/bar?bla=1'}});
     });
 
     it('defaults http method to GET', function() {
-      assert.deepEqual(apiCall.normalizeRequest({request: {path: '/foo/bar?bla=1'}}), {request: {method: 'GET', path: '/foo/bar?bla=1'}});
+      assert.deepEqual(apiCall.parseRequestString({request: {path: '/foo/bar?bla=1'}}), {request: {path: '/foo/bar?bla=1'}});
     });
   });
 
@@ -90,32 +102,60 @@ describe('api_call', function() {
     it('uses interpolation and array merge', function() {
       // Empty case - no interpolation, no array merge, no defaults
       assert.deepEqual(
-        apiCall.parse({request: {method: 'GET', path: 'bar'}}, {}),
-        {request: {method: 'GET', path: 'bar'}}
+        apiCall.parse({request: {method: 'GET', url: '/bar'}}, {}),
+        {request: {method: 'GET', url: '/bar'}}
       );
 
       // Only array merge
       assert.deepEqual(
-        apiCall.parse({request: {$merge: [{bar: 'bar', blu: 'blu'}, {bar: 'bla'}]}} , {}),
-        {request: {bar: 'bla', blu: 'blu'}}
+        apiCall.parse({request: {$merge: [{url: '/bar', method: 'DELETE'}, {url: '/bla'}]}} , {}),
+        {request: {url: '/bla', method: 'DELETE'}}
       );  
 
       assert.deepEqual(
         apiCall.parse(
-          {request: {method: 'GET', path: '/v1/users/{{users.member.id}}', headers: {$merge: [{"User-Id": "{{users.member.id}}"}, {"User-Name": "{{users.member.name}}"}]}}, response: {body: {equal: {name: "{{users.member.name}}"}}}},
+          {request: {method: 'GET', url: '/v1/users/{{users.member.id}}', headers: {$merge: [{"User-Id": "{{users.member.id}}"}, {"User-Name": "{{users.member.name}}"}]}}, response: {body: {equal: {name: "{{users.member.name}}"}}}},
           {data: {users: {member: {id: 404, name: 'Peter M'}}}}),
-        {request: {method: 'GET', path: '/v1/users/404', headers: {"User-Id": 404, "User-Name": "Peter M"}}, response: {body: {equal: {name: "Peter M"}}}}
+        {request: {method: 'GET', url: '/v1/users/404', headers: {"User-Id": 404, "User-Name": "Peter M"}}, response: {body: {equal: {name: "Peter M"}}}}
       );      
     });
 
     it('can interpolate arrays with integers', function() {
       assert.deepEqual(
         apiCall.parse(
-          {request: {method: 'GET', path: 'foo'}, response: {status: "{{status.invalid}}"}},
+          {request: {method: 'GET', url: 'foo'}, response: {status: "{{status.invalid}}"}},
           {data: {status: {invalid: [422, 400]}}}
         ),
-        {request: {method: 'GET', path: 'foo'}, response: {status: [422, 400]}}
+        {request: {method: 'GET', url: 'foo'}, response: {status: [422, 400]}}
       );
+    });
+
+    it('can parse request strings', function() {
+      var config = {defaults: {api_call: {request: {base_url: 'http://example.com'}}}};
+      assert.deepEqual(
+        apiCall.parse({request: '/foobar'}, {config: config}),
+        {request: {method: 'GET', url: 'http://example.com/foobar'}}
+      );
+
+      assert.deepEqual(
+        apiCall.parse({request: 'POST /foobar'}, {config: config}),
+        {request: {method: 'POST', url: 'http://example.com/foobar'}}
+      );
+
+      assert.deepEqual(
+        apiCall.parse({request: 'http://example.com/foobar'}, {}),
+        {request: {method: 'GET', url: 'http://example.com/foobar'}}
+      );
+
+      assert.deepEqual(
+        apiCall.parse({request: 'POST /foobar/{{user_id}}'}, {data: {user_id: 5}, config: config}),
+        {request: {method: 'POST', url: 'http://example.com/foobar/5'}}
+      );
+
+      assert.deepEqual(
+        apiCall.parse({request: '{{post_request}}'}, {data: {post_request: {method: 'POST', path: '/foobar/5'}}, config: config}),
+        {request: {method: 'POST', url: 'http://example.com/foobar/5'}}
+      );      
     });
 
     it('works with defaults', function() {
